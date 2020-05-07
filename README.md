825346f6c24833e83b732b94b660d7eee46f3910
ElSanty <vanegasdesarrollo@gmail.com>
2020-05-07 01:22:34 -0500

    Agregando el menu reponsive

---
+++ README.md
@@ -0,0 +1,5 @@
+# Web de la empresa H & S Linea de gas
+
+**Nota:** La siguiente web no esta alojada por decision de la           propietaria. 
+
+Puedes ver la pagina en el siguiente [link]().
--- javascript/script.js
+++ javascript/script.js
@@ -2,20 +2,42 @@
 
   'use strict';
   document.addEventListener('DOMContentLoaded', function(){
-//                                   Ubicacion del pin     zoom
+  //                                   Ubicacion del pin     zoom
     var map = L.map('mapa').setView([6.250907, -75.597503], 17);
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+  }).addTo(map);
           //Ubicacion del mapa
-L.marker([6.250907, -75.597503]).addTo(map)
+  L.marker([6.250907, -75.597503]).addTo(map)
     .bindPopup('H & S Linea de gas <br> Aqui estamos ubicados')
     .openPopup()
     .binTooltip('Un texto escondido (hover)')
     .openTooltip();
 
     //Ingresa todo el codigo JavaScript a partir de esta linea
+
+  });
+
+  var ban = 0;
+
+  $('.icono').on('click', function(){
+
+    if(ban == 1){
+
+      $('.nav-mobile').css({'left':'0'});
+      $('.icono').css({'color':'#F8E93B'});
+
+      ban = 0;
+
+    } else{
+
+      ban = 1;
+
+      $('.nav-mobile').css({'left':'-100%'});
+      $('.icono').css({'color':'#4760A4'});
+
+    }
 
   });
 
